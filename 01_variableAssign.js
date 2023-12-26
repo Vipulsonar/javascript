a6d4cd8db6078471c934b2f3edec5e6478e25b74
Vipulsonar <153152564+Vipulsonar@users.noreply.github.com>
2023-12-26 09:15:39 +0530

    01_varieavbleAssign.js added
    01_varieavbleAssign.js added

---
+++ 01_variableAssign.js
@@ -0,0 +1,22 @@
+var myFullName;
+console.log("Full Name:", myFullName);
+
+myFullName="Vipul Jadhav";
+console.log("Full Name:", myFullName);
+
+myFullName="Vipul Subhash Jadhav"
+console.log("Full Name:", myFullName);
+
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var fistName="Vipul", lastName="Sonar", age="24", address="Null", pincode="Null", gender="Male", isMarried="No";
+
+console.log("      Form          ");
+
+console.log("First Name    :", fistName)
+console.log("Last Name     :", lastName)
+console.log("Age           :", age)
+console.log("Pincode       :", pincode)
+console.log("Gender        :", gender)
+console.log("Marital Status:", isMarried)
--- helloWord.js
+++ helloWord.js
@@ -1,1 +1,38 @@
 console.log("Hello JS, I am Vipul");
+
+
+var num="10";
+console.log(num);
+
+var myName="Vipul"
+console.log(myName)
+
+
+console.log("First Name",myName)
+
+myName="Vipul Jadhav"
+console.log("Full Name",myName)
+
+var wifeName;
+wifeName="Null";
+console.log("Wife Name",wifeName);
+
+
+
+
+
+var n1 = "10";
+var n2 = "20";
+
+console.log("Before Swap:");
+console.log("n1 ->", n1);
+console.log("n2 ->", n2);
+
+var temp = n1;
+n1 = n2;
+n2 = temp;
+
+console.log("After Swap:");
+console.log("n1 ->", n1);
+console.log("n2 ->", n2);
+
