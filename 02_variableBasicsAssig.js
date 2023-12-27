fd4cc0e4b30495a03d7c189c6cd8f7d1820cb762
Vipulsonar <153152564+Vipulsonar@users.noreply.github.com>
2023-12-27 09:47:58 +0530

    02_variavbleBasicsAssig.js added
    02_variavbleBasicsAssig.js added

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,22 @@
+var myName="Vipul", lastName="Sonar", state="Maharashtra";
+
+var pin=411056, contact=9834896675, idNumber=1;
+
+var isMarried=false, pass=true, availbale=true;
+
+
+console.log("First Name:", myName, "," , "      Type:" ,typeof myName );
+console.log("Last Name: ", lastName, ",", "      Type:" ,typeof lastName );
+console.log("State Name:", state,",", "Type:" ,typeof state );
+
+console.log("");
+
+console.log("Pin:", pin, "," , "        Type:", typeof pin );
+console.log("Contact:", contact, "," , "Type:", typeof contact );
+console.log("ID Number:", idNumber, "," , "       Type:", typeof idNumber );
+
+console.log("");
+
+console.log("Married:", isMarried, ",", "          Type:", typeof isMarried);
+console.log("Pass:", pass, ",", "              Type:", typeof pass);
+console.log("Avilable for Class:", availbale, ",", "Type:", typeof availbale);
