8befc299123364cddc9acc283a50ce731c42ba3c
Vipulsonar <153152564+Vipulsonar@users.noreply.github.com>
2023-12-30 09:15:39 +0530

    Operators
    Operators

---
+++ 04_arithmaticOpr.js
@@ -0,0 +1,38 @@
+var n1=7;
+var n2=2;
+
+var resultn1n2= n1 + n2
+
+console.log(`+ ---> additiona is: ${resultn1n2} `);
+
+var resultn1n2= n1 - n2
+
+console.log(`- ---> Substraction is: ${resultn1n2} `);
+
+var resultn1n2= n1 * n2
+
+console.log(`* ---> Multiplication is: ${resultn1n2} `);
+
+var resultn1n2= n1 % n2
+
+console.log(`% ---> Modular or reminder is: ${resultn1n2} `);
+
+var resultn1n2= n1 / n2
+
+console.log(`/ ---> Division is: ${resultn1n2} `);
+
+var resultn1n2= n1 ** n2
+
+console.log(`** ---> Exponetion is: ${resultn1n2} `);
+
+var v=7
+v++;
+
+
+var num = 10;
+num += 2; //  num = num + 2;
+console.log(`+= Compund Addition is: ${num} `);
+
+var num = 10;
+num -= 5; //  num = num - 5;
+console.log(`+= Compund Substraction is: ${num} `);
---
+++ 04_operator.js
@@ -0,0 +1,7 @@
+var num= 5;
+var result = num % 2;
+resultValue = result == 0 ? "Given Number is Evevn": "It is odd number"
+
+console.log(`Even ODD : ${resultValue}`);
+
+
